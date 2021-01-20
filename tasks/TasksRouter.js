d2ed5cce3a9373d3127501974f63d79defc4324c
Mikhail Kleputin <kleputin333@gmail.com>
2021-01-20 18:53:43 +0300

    Added user authentication

--- tasks/TasksRouter.js
+++ tasks/TasksRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', auth, TasksController.get);
 router.delete('/delete/:id', auth, TasksController.delete);
 router.post('/add', auth, TasksController.addItem);
-router.delete('/delete_comp', TasksController.deleteComp);
+router.delete('/delete_comp', auth, TasksController.deleteComp);
 router.patch('/change_status', auth, TasksController.changeStatus);
 router.patch('/change_all', auth, TasksController.changeAll);
 router.patch('/change_name', auth, TasksController.changeName);
